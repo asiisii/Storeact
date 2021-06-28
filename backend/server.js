55eb11ce698faacc317482fd7857e91f1bcc917a
Ashish Malla <ashishmalla45@gmail.com>
2021-06-28 16:02:05 -0600

    Add simple server to get all products and single product

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const products = require('./data/products')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('API is running..')
+})
+
+//will give us the entire products
+app.get('/v1/products', (req, res) => {
+	// we could do res.json but its a json file so
+	res.json(products)
+})
+
+//this will give us the single product
+app.get('/v1/products/:id', (req, res) => {
+  // finds and returns products that matches the id 
+	const product = products.find(product => product._id === req.params.id)
+	res.json(product)
+})
+
+app.listen(5000, console.log('Server running on port 5000'))
