264e8a811e8a392cb50042b4ad5f883bb252b3d1
Ashish Malla <ashishmalla45@gmail.com>
2021-06-28 18:09:08 -0600

    Remove $ from description

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -39,7 +39,7 @@ const ProductPage = ({ match }) => {
 							/>
 						</ListGroup.Item>
 						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-						<ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
 					</ListGroup>
 				</Col>
 				<Col md={3}>
